fe62c42c54cac04c16094c9067d53ee9548ea81b
Joaovictor100 <joaovictorsts100@gmail.com>
2025-09-10 01:09:01 -0300

    Iniciando Arvores em Python

---
+++ scripts/advanced/BinaryTree/exemplo1.py
@@ -0,0 +1,14 @@
+
+
+class Node:
+    __init__(self, data):
+        self.node = data
+        self.left = None
+        self.right = None
+
+    __str__(self):
+        return str(self.node)
+
+class BinaryTree:
+    __init__(self):
+        
